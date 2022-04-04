144c2d83f26b4e83028c7c640b61dea7ad62d954
Ayesha Matwadia <ayeshamatwadia@gmail.com>
2022-04-04 20:30:37 +0200

    conditional rendering of elements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,11 +4,11 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 
-const  Expenses = (props) => {
+const Expenses = (props) => {
   const [year, setYear] = useState("2020");
-  
-  const filteredArray = props.expenses.filter(expense => {
-        return expense.date.getFullYear().toString() === year;
+
+  const filteredArray = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === year;
   });
 
   const yearUpdateHandler = (selectedYear) => {
@@ -23,14 +23,19 @@ const  Expenses = (props) => {
           selectedYear={year}
           onUpdateFilter={yearUpdateHandler}
         ></ExpensesFilter>
-        {filteredArray.map((expenseItem) => (
-          <ExpenseItem
-            key={expenseItem.id}
-            expenseItemDate={expenseItem.date}
-            expenseItemDescription={expenseItem.title}
-            expenseItemPrice={expenseItem.amount}
-          ></ExpenseItem>
-        ))}
+
+        {filteredArray.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredArray.map((expenseItem) => (
+            <ExpenseItem
+              key={expenseItem.id}
+              expenseItemDate={expenseItem.date}
+              expenseItemDescription={expenseItem.title}
+              expenseItemPrice={expenseItem.amount}
+            ></ExpenseItem>
+          ))
+        )}
       </Card>
     </div>
   );
